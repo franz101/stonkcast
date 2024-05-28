0e4936de37a1c84089386832f5cd7df3cb211ba8
franz <franz@rungalileo.io>
2024-05-29 00:10:17 +0200

    add subdomain

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "franz101.github.io/stonkcast",
+    baseUrl: "stonkcast.mioses.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
